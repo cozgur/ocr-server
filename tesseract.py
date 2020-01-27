0d784056a271e9cc7e498377547ffcd645fc9cee
ozang <ozan@pixerylabs.com>
2020-01-27 17:41:32 +0300

    small fix

--- tesseract.py
+++ tesseract.py
@@ -6,7 +6,7 @@ def pytesseract(filename, code):
 
     try:
         text = tess.image_to_string(Image.open(filename))
-        print("successfully detected")
+        print("successfully detected:\t" + text)
     except IOError as e:
         text = ''
         print("exception is thrown:\t " + e)
